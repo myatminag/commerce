2f0c228672bded54b5bf71c369b7d01176faa894
MyatMinAung <myatminaung.dev@gmail.com>
2024-08-10 15:20:15 +0630

    fix(import): update view type hook

--- apps/admin/src/app/(protected)/(brands)/brand-list/use-brand-list.ts
+++ apps/admin/src/app/(protected)/(brands)/brand-list/use-brand-list.ts
@@ -1,10 +1,10 @@
 import { useViewType } from '@hooks/use-view-type';
 
 export const useBrandList = () => {
-  const { viewType, handleToggleViewType } = useViewType();
+  const { viewType, createQueryString } = useViewType();
 
   return {
     viewType,
-    handleToggleViewType,
+    createQueryString,
   };
 };
