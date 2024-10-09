9aa01a747d659fe5f167bd781795aa37ad8e29c6
MyatMinAung <myatminaung.dev@gmail.com>
2024-10-09 16:54:11 +0630

    chore(eslint): downgrade eslint version to 8.57.0

--- apps/api/src/services/prisma/prisma.service.ts
+++ apps/api/src/services/prisma/prisma.service.ts
@@ -1,8 +1,19 @@
-import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
+import {
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
+import { Request } from "express";
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private clients: { [key: string]: PrismaClient } = {};
+
   async onModuleInit() {
     try {
       await this.$connect();
@@ -11,7 +22,34 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     }
   }
 
+  async getClient(req: Request) {
+    const tenantId = this.getTenantId(req);
+    let client = this.clients[tenantId];
+
+    if (!client) {
+      const databaseURL = process.env.DATABASE_URL!.replace("public", tenantId);
+
+      client = new PrismaClient({
+        datasources: {
+          db: {
+            url: databaseURL,
+          },
+        },
+      });
+
+      this.clients[tenantId] = client;
+    }
+
+    return client;
+  }
+
   async onModuleDestroy() {
-    await this.$disconnect();
+    await Promise.all(
+      Object.values(this.clients).map((client) => client.$disconnect()),
+    );
+  }
+
+  getTenantId(req: Request) {
+    return String(req.query.tenantId);
   }
 }
