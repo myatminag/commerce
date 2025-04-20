46611d00b5666fa172ccbccff64272f9888a6395
Myat Min Aung <myatminaung.dev@gmail.com>
2025-04-20 23:07:39 +0630

    fix(deps): correct tailwind css auto detection (#130)

--- apps/admin/src/app/(dashboard)/orders/components/filter.tsx
+++ apps/admin/src/app/(dashboard)/orders/components/filter.tsx
@@ -1,12 +1,72 @@
+"use client";
+
+import { SVGProps } from "react";
+import { RotateCwIcon } from "lucide-react";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@workspace/ui/components/select";
+import { Button } from "@workspace/ui/components/button";
 import { SearchInput } from "@workspace/ui/components/search-input";
+
+const StatusDot = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      width="8"
+      height="8"
+      fill="currentColor"
+      viewBox="0 0 8 8"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      {...props}
+    >
+      <circle cx="4" cy="4" r="4" />
+    </svg>
+  );
+};
 
 const Filter = () => {
   return (
-    <div className="flex items-center justify-between">
-      <SearchInput
-        placeholder="Search by order id or name..."
-        className="w-full lg:w-64"
-      />
+    <div className="flex items-start justify-between gap-x-3">
+      <div className="flex items-start gap-x-3">
+        <SearchInput placeholder="Search by name or sku..." />
+
+        <Select>
+          <SelectTrigger className="lg:w-32">
+            <SelectValue placeholder="Filter status" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="publish">
+                <span className="flex items-center gap-2">
+                  <StatusDot className="text-brand-600 size-2" />
+                  <span className="truncate">Publish</span>
+                </span>
+              </SelectItem>
+              <SelectItem value="draft">
+                <span className="flex items-center gap-2">
+                  <StatusDot className="text-danger-500 size-2" />
+                  <span className="truncate">Draft</span>
+                </span>
+              </SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+
+      <div className="flex items-center gap-x-3">
+        <Button size="lg">Search</Button>
+
+        <Button size="lg" variant="outline">
+          <RotateCwIcon />
+          Clear
+        </Button>
+      </div>
     </div>
   );
 };
--- apps/admin/src/app/(dashboard)/orders/page.tsx
+++ apps/admin/src/app/(dashboard)/orders/page.tsx
@@ -1,20 +1,13 @@
-import type { Metadata } from 'next';
-import { Suspense } from 'react';
+import type { Metadata } from "next";
 
-import OrderTable from './components/order-table';
+import OrderTable from "./components/order-table";
 
 export const metadata: Metadata = {
-  title: 'Orders',
+  title: "Orders",
 };
 
 const Page = () => {
-  return (
-    <div className="hidden h-full flex-1 flex-col space-y-4 p-6 lg:flex">
-      <Suspense>
-        <OrderTable />
-      </Suspense>
-    </div>
-  );
+  return <OrderTable />;
 };
 
 export default Page;
